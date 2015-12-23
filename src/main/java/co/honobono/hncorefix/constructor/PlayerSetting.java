9a2cb3115e85d2a1000b53d86c07649502ada0c5
syuchan1005 <@syu_chan_1005>
2015-12-23 22:32:03 +0900

    PlayerSetting内SettingComponentにgetter及びsetterを実装

--- src/main/java/co/honobono/hncorefix/constructor/PlayerSetting.java
+++ src/main/java/co/honobono/hncorefix/constructor/PlayerSetting.java
@@ -1,11 +1,18 @@
 package co.honobono.hncorefix.constructor;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.bukkit.entity.Player;
 
 public class PlayerSetting {
 
-	public PlayerSetting() {
+	List<SettingComponent> Components = new ArrayList<>();
 
+	public PlayerSetting() {}
+
+	public void setComponent(int x, int y, String name, String format) {
+		Components.add(new SettingComponent(x, y, name, format));
 	}
 
 	public void showWindow(Player player) {
@@ -15,16 +22,40 @@ public class PlayerSetting {
 }
 
 class SettingComponent {
-	int x;
-	int y;
-	String name;
-	String format;
-	Object value;
+	private int x;
+	private int y;
+	private String name;
+	private String format;
+	private Object value;
 
 	public SettingComponent(int x, int y, String name, String format) {
 		this.x = x;
 		this.y = y;
 		this.name = name;
 		this.format = format;
+	}
+
+	public Object getValue() {
+		return value;
+	}
+
+	public void setValue(Object value) {
+		this.value = value;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getFormat() {
+		return format;
 	}
 }
