d5e401adecd191108ec103551e09982efae020e7
syuchan1005 <@syu_chan_1005>
2015-12-23 23:13:34 +0900

    HNInventoryOutOfSlotExceptionを追加

---
+++ src/main/java/co/honobono/hncorefix/exception/HNInventoryOutOfSlotException.java
@@ -0,0 +1,11 @@
+package co.honobono.hncorefix.exception;
+
+import co.honobono.hncorefix.HNCoreFix;
+import net.md_5.bungee.api.ChatColor;
+
+public class HNInventoryOutOfSlotException extends Exception{
+
+	public HNInventoryOutOfSlotException(int slot) {
+		HNCoreFix.getInstance().getServer().getConsoleSender().sendMessage(ChatColor.RED + String.valueOf(slot) + "は指定範囲を超えています");
+	}
+}
