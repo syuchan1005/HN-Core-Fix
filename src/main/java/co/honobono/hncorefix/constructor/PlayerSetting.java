520f75857821d1e75782508e139a098d6f2e59ba
syuchan1005 <@syu_chan_1005>
2015-12-24 11:11:08 +0900

    getter, setterを追加

--- src/main/java/co/honobono/hncorefix/constructor/PlayerSetting.java
+++ src/main/java/co/honobono/hncorefix/constructor/PlayerSetting.java
@@ -1,9 +1,13 @@
 package co.honobono.hncorefix.constructor;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
 
 import co.honobono.hncorefix.exception.HNInventoryOutOfSlotException;
 
@@ -11,9 +15,10 @@ public class PlayerSetting {
 
 	private String Title;
 	private List<SettingComponent> Components = new ArrayList<>();
+	private Map<Player, Map<String, Object>> map = new HashMap<>();
 
 	public PlayerSetting(String Title) {
-		this.Title = Title;
+		this.Title = ChatColor.RESET + Title;
 	}
 
 	public void setComponent(int x, int y, String name, String format) throws HNInventoryOutOfSlotException {
@@ -29,13 +34,26 @@ public class PlayerSetting {
 
 	}
 
+	public String getTitle() {
+		return this.Title;
+	}
+
+	public Map<String, Object> getSetting(Player player) {
+		if(map.containsKey(player)) {
+			return map.get(player);
+		}
+		return null;
+	}
+
+	public void setSetting(Inventory inv) {
+
+	}
 }
 
 class SettingComponent {
 	private int slot;
 	private String name;
 	private String format;
-	private Object value;
 
 	public SettingComponent(int slot, String name, String format) {
 		this.slot = slot;
@@ -47,14 +65,6 @@ class SettingComponent {
 		this.slot = x + (y - 1) * 9;
 		this.name = name;
 		this.format = format;
-	}
-
-	public Object getValue() {
-		return value;
-	}
-
-	public void setValue(Object value) {
-		this.value = value;
 	}
 
 	public int getSlot() {
