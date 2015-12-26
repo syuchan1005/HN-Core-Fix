5089a550be93c4e4cc3013d7ef5deddc52f4a274
syuchan1005 <@syu_chan_1005>
2015-12-26 18:13:03 +0900

    viewregのParticleを変更

--- src/main/java/co/honobono/hncorefix/command/viewreg.java
+++ src/main/java/co/honobono/hncorefix/command/viewreg.java
@@ -15,17 +15,20 @@ import co.honobono.hncorefix.HNCoreFix;
 import co.honobono.hncorefix.annotation.AddCommand;
 import co.honobono.hncorefix.util.Util;
 import net.minecraft.server.v1_8_R3.EnumParticle;
+import net.minecraft.server.v1_8_R3.PacketPlayOutWorldParticles;
 
 public class viewreg {
 
 	@SuppressWarnings("deprecation")
-	@AddCommand(command = {"viewreg" }, description = "This is View in Region Command.", permission = "hncorefix.viewreg")
+	@AddCommand(command = {
+			"viewreg" }, description = "This is View in Region Command.", permission = "hncorefix.viewreg")
 	public boolean c(CommandSender sender, String[] args) {
 		if (!(sender instanceof Player)) {
 			sender.sendMessage("ゲーム内から実行して下さい.");
 			return true;
 		}
-		if(args.length < 2) return false;
+		if (args.length < 2)
+			return false;
 		Player player = (Player) sender;
 		ProtectedRegion protect = WGBukkit.getRegionManager(player.getWorld()).getRegion(args[1]);
 		BlockVector min = protect.getMinimumPoint();
@@ -36,12 +39,17 @@ public class viewreg {
 					Location loc = new Location(player.getWorld(), x, y, z);
 					player.sendBlockChange(loc, Material.AIR, (byte) 0);
 					Util.normalParticle(player, EnumParticle.REDSTONE, loc, 3);
-					Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(HNCoreFix.getInstance(), new BukkitRunnable() {
-						@Override
-						public void run() {
-							player.sendBlockChange(loc, loc.getBlock().getType(), (byte) 0);
-						}
-					}, 100L);
+					Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(HNCoreFix.getInstance(),
+							new BukkitRunnable() {
+								@Override
+								public void run() {
+									player.sendBlockChange(loc, loc.getBlock().getType(), (byte) 0);
+								}
+							}, 100L);
+					PacketPlayOutWorldParticles packet = (x % 2 == 0 && y % 2 == 0 && z % 2 == 0) ? new PacketPlayOutWorldParticles(EnumParticle.REDSTONE, false, (float) (x + 0.5D),
+							(float) (y + 0.5D), (float) (z + 0.5D), 0, 0, 0, 10.0F, 0, new int[] { 0 }) : new PacketPlayOutWorldParticles(EnumParticle.REDSTONE, false, (float) (x + 0.5D),
+									(float) (y + 0.5D), (float) (z + 0.5D), 255, 242, 0, 10.0F, 0, new int[] { 0 });
+					Util.sendPlayerPacket(player, packet);
 				}
 			}
 		}
