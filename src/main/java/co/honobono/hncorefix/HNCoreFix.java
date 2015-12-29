89fe64e985cd0a53331e96a99343571176d4e0d2
syuchan1005 <@syu_chan_1005>
2015-12-29 11:39:35 +0900

    DBファイルの処理を追加

--- src/main/java/co/honobono/hncorefix/HNCoreFix.java
+++ src/main/java/co/honobono/hncorefix/HNCoreFix.java
@@ -10,6 +10,7 @@ import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import co.honobono.hncorefix.command.Look;
 import co.honobono.hncorefix.constructor.CommandManager;
 import co.honobono.hncorefix.util.Config;
 
@@ -27,6 +28,7 @@ public class HNCoreFix extends JavaPlugin {
 		pc.setExecutor(this);
 		pc.setTabCompleter(manager);
 		this.saveDefaultConfig();
+		Look.checkDB();
 		try {
 			config = Config.getConfig(new File(this.getDataFolder(), "config.yml"));
 			Load.Register(this, manager);
